871643247f91213605b1935af3c05c87ddb6d077
Vlad <satogob75@gmail.com>
2023-03-19 16:27:15 +0600

    first commit

---
+++ src/test/java/com/example/kurzwork/KurzworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kurzwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurzworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
