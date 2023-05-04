d810614c3d2b00e0d144e67078d382a7e65474e6
Vlad <satogob75@gmail.com>
2023-05-04 16:03:43 +0600

    фото

--- src/main/java/com/example/kurzwork/Controllers/catalog_controller.java
+++ src/main/java/com/example/kurzwork/Controllers/catalog_controller.java
@@ -37,8 +37,15 @@ public class catalog_controller {
     public String getAllCatalog(Model model) {
         Iterable<Catalog> catalog = catalogRepository.findAll();
         model.addAttribute("catalog", catalog);
-        //catalogRepository.save(new Catalog("ffff", "fdsaf", "https://avatars.mds.yandex.net/i?id=329a652c9e349faef992e77a18534d0bd8471613-8210080-images-thumbs&n=13", "car", "1000"));
-        //catalogRepository.save(new Catalog("аааа", "fdsaf", "https://avatars.mds.yandex.net/i?id=329a652c9e349faef992e77a18534d0bd8471613-8210080-images-thumbs&n=13", "car", "1000"));
+        //catalogRepository.save(new Catalog("e-Lavida", "Volkswagen e-Lavida is a European C—class sedan, which in the third generation shows a significant step forward in terms of exterior style, quality and luxury of the interior compared to its predecessor.", "http://localhost:8080/1.png", "Sedan", "$10000"));
+        //catalogRepository.save(new Catalog("Volkswagen Jetta", "The seventh-generation Volkswagen Jetta sedan was unveiled at the North American International Auto Show in Detroit in January 2018.", "http://localhost:8080/2.png", "Sedan", "$10025"));
+        //catalogRepository.save(new Catalog("Volkswagen Passat", "Volkswagen Passat 2020 model year received a slightly modified design and new items in terms of technology. The optics, bumpers and front grille have been updated, although all of them have generally retained the same style.", "http://localhost:8080/3.png", "Universal", "$20000"));
+        //catalogRepository.save(new Catalog("Volkswagen Phaeton", "The Volkswagen Phaeton of the first generation has been produced since 2002. Phaeton is a representative of the premium segment, its luxurious interior is equipped, to choose from, with a solid rear sofa or two separate rear seats.", "http://localhost:8080/4.png", "Sedan", "$10558"));
+        //catalogRepository.save(new Catalog("Volkswagen Passat CC", "Volkswagen Passat CC is a four—door comfort coupe built on the basis of the sixth-generation Passat. The model debuted in early 2008.", "http://localhost:8080/5.png", "Sedan", "$20054"));
+        //catalogRepository.save(new Catalog("Volkswagen Saveiro", "Volkswagen Saveiro passenger pickup truck, sold in South American markets, is based on the Gol/Pointer model, which is popular in these countries.", "http://localhost:8080/6.png", "Truck", "$30000"));
+        //catalogRepository.save(new Catalog("Taos", "Compact crossover Taos in the product hierarchy of the Volkswagen brand will stand a step below Tiguan and will compete with such market hits as Hyundai Creta, Kia Seltos and Renault Duster.", "http://localhost:8080/7.png", "Universal", "$32000"));
+        //catalogRepository.save(new Catalog("Terramont", "The premiere of the restyled Terramont took place in February 2020 in the USA, where it is called Atlas. From the outside of the car of the previous model, it differs in the modified design of the front part, as well as another rear bumper.", "http://localhost:8080/8.png", "SUV", "$40000"));
+        //catalogRepository.save(new Catalog("Volkswagen Tiguan", "The Volkswagen Tiguan of the second generation has been produced since 2016. The upgraded version was introduced in 2020. The design of the front and rear parts of the body has been changed.", "http://localhost:8080/9.png", "Hatchback", "$35000"));
 
         return "catalog";
     }
--- src/main/java/com/example/kurzwork/Controllers/home_controller.java
+++ src/main/java/com/example/kurzwork/Controllers/home_controller.java
@@ -17,9 +17,9 @@ public class home_controller {
     public String getKarysel(Model model) {
         Iterable<Karysel> karysel = karyselRepository.findAll();
         model.addAttribute("karysel", karysel);
-        //karyselRepository.save(new Karysel("https://avatars.mds.yandex.net/i?id=329a652c9e349faef992e77a18534d0bd8471613-8210080-images-thumbs&n=13"));
-        //karyselRepository.save(new Karysel("https://avatars.mds.yandex.net/i?id=6b0bdad1c0d1bb319cf5b01716cd3b17b8300b1a-7552332-images-thumbs&n=13"));
-
+        //karyselRepository.save(new Karysel("1.jpg"));
+        //karyselRepository.save(new Karysel("2.jpg"));
+        //karyselRepository.save(new Karysel("3.jpg"));
         return "home";
     }
 }
